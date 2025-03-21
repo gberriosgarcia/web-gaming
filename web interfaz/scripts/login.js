c9662179428bf00e82183db891deeba3c57d957d
katferreiraduoc <kat.ferreira@duocuc.cl>
2025-03-20 22:40:10 -0500

    carga logica base js

--- web interfaz/scripts/login.js
+++ web interfaz/scripts/login.js
@@ -1,1 +1,38 @@
-console.log("Hola mundo")
+var nombre = "";
+var usuario = "";
+var email = "";
+var nacimiento = "";
+var clave = "";
+var reingreso_clave = "";
+var direccion = "";
+
+function validarVaciosNulos(campo){
+    if(campo === null || campo.length === 0){
+        alert("Campo vacio")
+    };
+};
+
+function guardarDatos(){
+    console.log("paso");
+    nombre = document.getElementById('nombre').value;
+    usuario= document.getElementById('usuario').value;
+    email = document.getElementById('email').value;
+    nacimiento = document.getElementById('nacimiento').value;
+    direccion = document.getElementById('direccion').value;
+    clave = document.getElementById('clave').value;
+    reingreso_clave = document.getElementById('reingreso_clave').value;
+    
+    validarVaciosNulos(nombre);
+
+    if(clave != reingreso_clave){
+        alert("Contraseña invalida")
+    };
+    console.log(nombre);
+    console.log(usuario);
+    console.log(email);
+    console.log(nacimiento);
+    console.log(direccion);
+    console.log(clave);
+    console.log(reingreso_clave);
+};
+
